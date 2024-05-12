fe24d7b1d04d1a816832dd6c668b5a9a80af5ce6
~hedy <hedy.dev@protonmail.com>
2024-05-12 16:44:38 +0800

    Update todo

--- src/App.tsx
+++ src/App.tsx
@@ -24,26 +24,10 @@ const App: Component = () => {
 };
 
 /* TODO
- * ALL DONE FOR NOW
  *
- * Decision:
- * X Use solid-js/store for a single decision
- * X On each input field change, update store
- * X Buttons for adding rows/cols:
- *   X Will update relevant arrays in store
- *
- * ~~"Debug" button
- *   X For now, use the `show` to display a list of rank calculations!
- *   So it's actually usable!
- *
- * App:
- * X Use store for an array of decision (IDs?)
- * X "Add decision" will append to this array
- * X Possible need of sharing context for decisiosn between App and Decision
- * X Use the array of known decisions, and the context, to produce the jump list
- *
- * Edit option and factor names
- * Better cleaner interface
+ * When gearing:
+ *   Make option names and factor names into inputs, onChange updates
+ * Gear button feedback for whether current state is gearing or not
  *
  * */
 
